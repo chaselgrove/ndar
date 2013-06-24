ff24715d2123393626d472c7ecd302a57c3e7f45
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2013-06-24 20:44:50 +0000

    unpacking Image.files and S3Image.files when accessed

--- ndar.py
+++ ndar.py
@@ -5,7 +5,7 @@ import csv
 import shutil
 import zipfile
 try:
-    import boto.s3.connection
+    from boto.s3.connection import OrdinaryCallingFormat, S3Connection
     import boto.s3.key
 except ImportError:
     pass
@@ -13,6 +13,12 @@ try:
     import MySQLdb
 except ImportError:
     pass
+
+def NDARError(Exception):
+    """base class for exceptions"""
+
+def ObjectNotFoundError(NDARError):
+    """S3 object not found"""
 
 def _get_file_type(fname):
     """Return the type of a file."""
@@ -34,7 +40,7 @@ def _get_file_type(fname):
             return 'DICOM'
     return 'other'
 
-class _BaseImage:
+class _BaseImage(object):
 
     """base class for images"""
 
@@ -51,13 +57,13 @@ class _BaseImage:
 
         os.mkdir('%s/unpacked' % self._tempdir)
         self.files = {'DICOM': [], 
-                          'NIfTI-1': [], 
-                          'MINC': [], 
-                          'BRIK': [], 
-                          'NRRD': [], 
-                          'PNG': [], 
-                          'JPEG': [], 
-                          'other': []}
+                      'NIfTI-1': [], 
+                      'MINC': [], 
+                      'BRIK': [], 
+                      'NRRD': [], 
+                      'PNG': [], 
+                      'JPEG': [], 
+                      'other': []}
         if self.source.endswith('.zip'):
             zf = zipfile.ZipFile(self._temp_source)
             zf.extractall('%s/unpacked' % self._tempdir)
@@ -111,11 +117,18 @@ class Image(_BaseImage):
         if not os.path.exists(self.source):
             raise IOError(errno.ENOENT, 
                           "No such file or directory: '%s'" % self.source)
-        self._source_base = os.path.basename(self.source)
-        self._temp_source = '%s/%s' % (self._tempdir, self._source_base)
-        os.symlink(self.source, self._temp_source)
-        self._unpack()
+        self.files = None
         return
+
+    def __getattribute__(self, name):
+        value = _BaseImage.__getattribute__(self, name)
+        if name == 'files' and value is None:
+            self._source_base = os.path.basename(self.source)
+            self._temp_source = '%s/%s' % (self._tempdir, self._source_base)
+            os.symlink(self.source, self._temp_source)
+            self._unpack()
+            return self.files
+        return value
 
 class S3Image(_BaseImage):
 
@@ -126,21 +139,42 @@ class S3Image(_BaseImage):
         if 'boto' not in sys.modules:
             raise ImportError('boto S3 connection module not found')
         self.source = source
+        self._s3_access_key = s3_access_key
+        self._s3_secret_key = s3_secret_key
+        # even though we don't actually download the object here, we check 
+        # it so exceptions are raised at instantiation
         # source is 's3://bucket/path/to/object'
-        (bucket_name, object_name) = source[5:].split('/', 1)
-        calling_format = boto.s3.connection.OrdinaryCallingFormat()
-        s3 = boto.s3.connection.S3Connection(s3_access_key, 
-                                             s3_secret_key, 
-                                             calling_format=calling_format)
+        (bucket_name, object_name) = self.source[5:].split('/', 1)
+        s3 = S3Connection(self._s3_access_key, 
+                          self._s3_secret_key, 
+                          calling_format=OrdinaryCallingFormat())
         bucket = s3.get_bucket(bucket_name)
         key = boto.s3.key.Key(bucket)
         key.key = object_name
-        self._source_base = os.path.basename(self.source)
-        self._temp_source = '%s/%s' % (self._tempdir, self._source_base)
-        key.get_contents_to_filename(self._temp_source)
+        if not key.exists():
+            raise ObjectNotFoundError()
         s3.close()
-        self._unpack()
+        self.files = None
         return
+
+    def __getattribute__(self, name):
+        value = _BaseImage.__getattribute__(self, name)
+        if name == 'files' and value is None:
+            # source is 's3://bucket/path/to/object'
+            (bucket_name, object_name) = self.source[5:].split('/', 1)
+            s3 = S3Connection(self._s3_access_key, 
+                              self._s3_secret_key, 
+                              calling_format=OrdinaryCallingFormat())
+            bucket = s3.get_bucket(bucket_name)
+            key = boto.s3.key.Key(bucket)
+            key.key = object_name
+            self._source_base = os.path.basename(self.source)
+            self._temp_source = '%s/%s' % (self._tempdir, self._source_base)
+            key.get_contents_to_filename(self._temp_source)
+            s3.close()
+            self._unpack()
+            return self.files
+        return value
 
 class _BasePackage:
 
