70761d6f33a590380f9493dc71db85046e042760
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2014-07-16 14:06:30 -0400

    dashboard work with summary data

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -25,9 +25,89 @@ class Summary:
     def __init__(self):
         with DB() as c:
             c.execute("SELECT * FROM summary")
-            c.arraysize = 1000
-            cols = [ el[0] for el in c.description ]
-            self.rows = [ dict(zip(cols, row)) for row in c ]
+            cols = [ el[0].lower() for el in c.description ]
+            self.all = []
+            self.multiples = []
+            self.modalities = {'MR': [], 'Other': []}
+            self.classes = {'Structural': [], 
+                            'Time Series': [], 
+                            'Diffusion': [], 
+                            'Other': []}
+            self.types = {'MR structural (MPRAGE)': [], 
+                          'MR structural (T1)': [], 
+                          'MR structural (PD)': [], 
+                          'MR structural (FSPGR)': [], 
+                          'fMRI': [], 
+                          'MR diffusion': [], 
+                          'MR structural (T2)': [], 
+                          'Other': []}
+            for row in c:
+                d = dict(zip(cols, row))
+                self.all.append(d)
+                if d['n_image03'] > 1:
+                    d['class'] = None
+                    self.multiples.append(d)
+                else:
+                    if d['image_modality'] == 'MR':
+                        self.modalities['MR'].append(d)
+                        if d['scan_type'] in ('MR structural (MPRAGE)', 
+                                              'MR structural (T1)', 
+                                              'MR structural (FSPGR)'):
+                            d['class'] = 'Structural'
+                            self.classes['Structural'].append(d)
+                            self.types[d['scan_type']].append(d)
+                        elif d['scan_type'] == 'fMRI':
+                            d['class'] = 'Time Series'
+                            self.classes['Time Series'].append(d)
+                            self.types[d['scan_type']].append(d)
+                        elif d['scan_type'] == 'MR diffusion':
+                            d['class'] = 'Diffusion'
+                            self.classes['Diffusion'].append(d)
+                            self.types[d['scan_type']].append(d)
+                        elif d['scan_type'] in ('MR structural (T2)', 
+                                                'MR structural (PD)'):
+                            d['class'] = 'Other'
+                            self.classes['Other'].append(d)
+                            self.types[d['scan_type']].append(d)
+                        else:
+                            d['class'] = 'Other'
+                            self.classes['Other'].append(d)
+                            self.types['Other'].append(d)
+                    else:
+                        self.modalities['Other'].append(d)
+        return
+
+    def split(self, image_modality=None, image_class=None, image_type=None):
+        if image_class:
+            (okay_l, error_l) = self._split(self.classes[image_class])
+        elif image_type:
+            (okay_l, error_l) = self._split(self.types[image_type])
+        elif image_modality:
+            (okay_l, error_l) = self._split(self.modalities[image_type])
+        else:
+            (okay_l, error_l) = self._split(self.all)
+        return (okay_l, error_l)
+
+    def _split(self, l):
+        # _split(list) -> (okay list, error list)
+        okay_l = []
+        error_l = []
+        for d in l:
+            if not d['has_basic_check']:
+                error_l.append(d)
+                continue
+            if not d['has_thumbnail']:
+                error_l.append(d)
+                continue
+            if not d['has_derived_image03']:
+                error_l.append(d)
+                continue
+            if d['class'] == 'Structural' and not d['has_structural_qa']:
+                error_l.append(d)
+                continue
+            if d['class'] == 'Time Series' and not d['has_time_series_qa']:
+                error_l.append(d)
+                continue
         return
 
 class Volume:
@@ -66,14 +146,14 @@ db_dsn = cx_Oracle.makedsn(os.environ['DB_HOST'],
 
 app = flask.Flask(__name__)
 
+import time
+t0 = time.time()
 summary = Summary()
+print time.time() - t0
 
 @app.route('/')
 def index():
-    return flask.render_template('index.tmpl', 
-                                 unchecked=unchecked, 
-                                 multiples=multiples, 
-                                 not_mri=not_mri)
+    return flask.render_template('index.tmpl', summary=summary)
 
 @app.route('/volume/<path:spec>')
 def volume(spec):
